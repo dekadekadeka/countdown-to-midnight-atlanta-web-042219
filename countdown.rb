4bfae59cf99069549651e08671db2269c1268dd1
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-01 18:43:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
 #write your code here
 
 def countdown(number)
-  while number < 0
+  num = number
+  while num > 0
     puts "#{number} SECOND(S)!"
+    num -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
